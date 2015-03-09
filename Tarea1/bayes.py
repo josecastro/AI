976666836a056fbb66a1ea57670846de299bf5bd
Jose Navas Su <jnavas05@gmail.com>
2015-03-08 22:42:06 -0600

    Method marginalize added to Factor

--- Tarea1/bayes.py
+++ Tarea1/bayes.py
@@ -29,58 +29,59 @@ class Variable:
         output += "{'%s','%s',%s,%s}" % (self.name,self.description,self.cardinality,self.domain)
         return output
 
-def __map_vars_values__(vars, card):
-    # make sequences values array
-    sequences = array([asarray(v.domain) for v in vars])
-    # construct an open mesh from sequences
-    broadcastable = ix_(*sequences)
-    # broadcast arrays against each other
-    broadcasted = broadcast_arrays(*broadcastable)
-    # rows = product of cardinalities, cols = number of sequences
-    rows, cols = reduce(multiply, broadcasted[0].shape), len(broadcasted)
-    # empty target array
-    out = empty(rows * cols, dtype=broadcasted[0].dtype)
-    start, end = 0, rows
-    # flatten one sequence at a time
-    for a in broadcasted:
-        # flatten sequence dimension
-        out[start:end] = a.reshape(-1)
-        start, end = end, end + rows
-    # reshape, transpose and return each product concatenated as a string
-    keys = map("".join, out.reshape(cols, rows).T)
-    # create map from keys to values indices
-    return dict(zip(keys, array(arange(0,card))))
-
 # Factor: function from a list of variables to a numeric value
 class Factor:
     # __init__ constructor
-    # pending
-    #     if values == [], then create vector with 0's
-    #     initialize map, map['T'+'Good'] -> value (depending on variables)
     #
     # 1. Variables order is relevant
-    # 2. Variables are stored in vector named vars
+    # 2. Variables are stored in vector named variables
     # 3. Variables cardinality are stored in vector named cardinality
     # 4. Values shall be implemented as a vector (eventually numpy array)
-    # 5. A dictionary shall be stored in map, that maps factor names to values indices
+    # 5. A dictionary shall be stored in map, that maps variables names to values indices
+    #    map['T'+'Good'] -> value (depending on variables)
     # 6. Values shall be initialized to 0's
-    def __init__(self, vars=[], values=[]):
+    def __init__(self, variables=[], values=[]):
         # save variables
-        self.vars = array(vars)
+        self.variables = array(variables)
+        # initialize variables names array
+        self.variable_names = array([v.name for v in variables])
         # initialize cardinality array
-        self.cardinality = array([v.cardinality for v in vars])
+        self.cardinality = array([v.cardinality for v in variables])
         card = prod(self.cardinality)
         # implement as numpy array right filled with zeroes
         self.values = zeros(card)
         max_values = min(card, len(values))
         self.values[0:max_values] = array(values[0:max_values])
         # map variable names to variable values
-        self.map = __map_vars_values__(self.vars, card)
+        self.map = self.__map_vars_values__(self.variables)
+
+    # map variables names to values indices
+    def __map_vars_values__(self, variables):
+        # make sequences values array
+        sequences = array([asarray(v.domain) for v in variables])
+        # construct an open mesh from sequences
+        broadcastable = ix_(*sequences)
+        # broadcast arrays against each other
+        broadcasted = broadcast_arrays(*broadcastable)
+        # rows = product of cardinalities, cols = number of sequences
+        rows, cols = reduce(multiply, broadcasted[0].shape), len(broadcasted)
+        # empty target array
+        out = empty(rows * cols, dtype=broadcasted[0].dtype)
+        start, end = 0, rows
+        # flatten one sequence at a time
+        for a in broadcasted:
+            # flatten sequence dimension
+            out[start:end] = a.reshape(-1)
+            start, end = end, end + rows
+        # reshape, transpose and return each product concatenated as a string
+        keys = map("".join, out.reshape(cols, rows).T)
+        # create map from keys to values indices
+        return dict(zip(keys, array(arange(0, rows))))
 
     def __str__(self):
         output = ""
         output += "[Factor]\n"
-        output += "Vars:       \t%s\n" % [v.name for v in self.vars]
+        output += "Variables:  \t%s\n" % [v.name for v in self.variables]
         output += "Values:     \t%s\n" % self.values
         output += "Map:        \t%s\n" % self.map
         output += "Cardinality:\t%s\n" % self.cardinality
@@ -91,13 +92,16 @@ class Factor:
 
     def toString(self):
         output = ""
-        output += "{'%s','%s',%s,%s}" % ([v.name for v in self.vars],self.values,self.map,self.cardinality)
+        output += "{'%s', '%s', %s, %s}" % ([v.name for v in self.variables], self.values, self.map, self.cardinality)
         return output
 
     # this function should sum over the variables in varList and
     # return the resulting factor
     def marginalize(self, varList):
-        return Factor()
+        indeces = sort(asarray([nonzero(self.variable_names == idx)[0][0] for idx in varList]))[::-1]
+        reshaped_values = self.values.reshape(self.cardinality)
+        marginalizedFactor = Factor(delete(self.variables, indeces), map(lambda idx: sum(reshaped_values, axis = idx), indeces)[0].tolist())
+        return marginalizedFactor
 
     # retrieve the value of factor for a given combinations of values 
     # of the variables from the factor array
@@ -109,12 +113,12 @@ class Factor:
     def getValueIdx(self, idxList):
         return 0
 
-    # see getValue
-    def setValue(self, values,val):
+    # see setValue
+    def setValue(self, values, val):
         return 0
 
-    # see getValueIdx
-    def setValueIdx(self, valIdx,val):
+    # see setValueIdx
+    def setValueIdx(self, valIdx, val):
         return 0
 
 
@@ -145,45 +149,45 @@ def jointDistribution(factors):
     return Factor()
 
 # marginal computes the marginal over a set of given variables
-#   M = marginal(vars, factors, evidence) computes the marginal over variables
-#   vars in the distribution induced by the set of factors, given the evidence
+#   M = marginal(variables, factors, evidence) computes the marginal over variables
+#   variables in the distribution induced by the set of factors, given the evidence
 #
-def marginal(vars, factors, evidence):
+def marginal(variables, factors, evidence):
     return Factor()
 
 class Net:
-    def __init__(self, name='', vars=[], factors=[], JSONfile=''):
+    def __init__(self, name='', variables=[], factors=[], JSONfile=''):
         self.name = name
-        self.vars = vars
+        self.variables = variables
         self.factors = factors
         self.JSONfile = JSONfile
 
-    def setVariables(variables):
-        self.vars = variables
+    def setVariables(self, variables):
+        self.variables = variables
 
-    def setFactors(factors):
-        self.factorts = fators
+    def setFactors(self, factors):
+        self.factorts = factors
 
-    def marginal(vars, evidence):
-        return marginal(vars, self.factors, evidence)
+    def marginal(self, variables, evidence):
+        return marginal(variables, self.factors, evidence)
 
     def priorSample(self):
         # algorithm PRIOR-SAMPLE(bn) in chapter 14 of AIMA book
         return []
 
-    def rejectionSampling(varNameVector, evidence, numTries):
+    def rejectionSampling(self, varNameVector, evidence, numTries):
         # algorithm REJECTION-SAMPLING(X,e,bn,N) in 
         # chapter 14 of AIMA book
         
         return []
 
-    def likelihoodWeighing(varNameVector, evidence, numTries):
+    def likelihoodWeighing(self, varNameVector, evidence, numTries):
         # algorithm LIKELIHOOD-WEIGHING(X,e,bn,N) in
         # chapter 14 of AIMA book
         
         return []
 
-    def markovChainMonteCarlo(varNameVector, evidence, numTries):
+    def markovChainMonteCarlo(self, varNameVector, evidence, numTries):
         # algorithm MCMC-ASK(X,e,bn,N) in
         # chapter 14 of AIMA book
 
--- Tarea1/test.py
+++ Tarea1/test.py
@@ -13,19 +13,19 @@ x3 = bayes.Variable(name='X3', domain=['high', 'low'])
 factor = {}
 
 # factor contains P[X1]
-factor['p(x1)'] = bayes.Factor(vars=[x1], values=[0.11, 0.89])
+factor['p(x1)'] = bayes.Factor(variables=[x1], values=[0.11, 0.89])
 
 # factor contains P[X2|X1]
-factor['p(x2|x1)'] = bayes.Factor(vars=[x2,x1], values=[0.59, 0.41, 0.22, 0.78])
+factor['p(x2|x1)'] = bayes.Factor(variables=[x2,x1], values=[0.59, 0.41, 0.22, 0.78])
 
 # factor contains P[X3|X2]
-factor['p(x3|x2)'] = bayes.Factor(vars=[x3,x2], values=[0.39, 0.61, 0.06, 0.94])
+factor['p(x3|x2)'] = bayes.Factor(variables=[x3,x2], values=[0.39, 0.61, 0.06, 0.94])
 
 # factors['product'] = factorProduct(factor['p(x1)'], factor['p(x2|x1)'])
 factor['product'] = bayes.Factor([x1,x2], [0.0649, 0.01958, 0.0451, 0.6942])
 
-# factors['marginal1'] = factor['p(x2|x1)'].marginalize(['X2'])
-factor['marginal1'] = bayes.Factor(vars=[x1], values=[1.0, 1.0])
+factor['marginal1a'] = factor['p(x2|x1)'].marginalize(['X2'])
+factor['marginal1b'] = bayes.Factor(variables=[x1], values=[0.81, 1.19])
 
 # [factor['e1'], factor['e2'], factor['e3']] = 
 #              bayes.observeEvidence([factor['p(x1)'], 
@@ -33,15 +33,15 @@ factor['marginal1'] = bayes.Factor(vars=[x1], values=[1.0, 1.0])
 #                                     factor['p(x3|x2)']], 
 #                                     {'X2':'0', 'X3':'low'})
 
-factor['e1'] = bayes.Factor(vars=[x1], values=[0.11, 0.89])
-factor['e2'] = bayes.Factor(vars=[x2,x1], values=[0.59, 0.0, 0.22, 0.0])
-factor['e3'] = bayes.Factor(vars=[x3,x2], values=[0.0, 0.61, 0.0, 0.0])
+factor['e1'] = bayes.Factor(variables=[x1], values=[0.11, 0.89])
+factor['e2'] = bayes.Factor(variables=[x2,x1], values=[0.59, 0.0, 0.22, 0.0])
+factor['e3'] = bayes.Factor(variables=[x3,x2], values=[0.0, 0.61, 0.0, 0.0])
 
 # factor['joint'] = bayes.jointDistribution([factor['p(x1)'], 
 #                                            factor['p(x2|x1)'],
 #                                            factor['p(x3|x2)']])
 
-factor['joint'] = bayes.Factor(vars= [x1,x2,x3], values=[0.025311, 0.076362, 
+factor['joint'] = bayes.Factor(variables= [x1,x2,x3], values=[0.025311, 0.076362, 
                                                          0.002706, 0.041652,
                                                          0.039589, 0.119438,
                                                          0.042394, 0.652548])
